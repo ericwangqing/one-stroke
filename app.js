a5ffc6fde4ff6e644e9804df94be90d259bd9d47
wangqing <wangqing@mq-ai.cn>
2023-04-11 13:49:54 +0800

    重构，拆分Graph和App

--- app.js
+++ app.js
@@ -1,125 +1,17 @@
-const canvas = document.getElementById('drawingArea');
-const ctx = canvas.getContext('2d');
+import {Edge, Graph, Vertex} from "./graph";
+
+
 const restartDrawingBtn = document.getElementById('restartDrawing');
 const restartGraphBtn = document.getElementById('restartGraph');
-
-class Graph {
-  constructor() {
-    this.vertices = [];
-    this.edges = [];
-  }
-
-  addVertex(vertex) {
-    this.vertices.push(vertex);
-  }
-
-  addEdge(edge) {
-    this.edges.push(edge);
-  }
-
-  // 其他方法
-}
-
-class Vertex {
-  constructor(x, y, name) {
-    this.x = x;
-    this.y = y;
-    this.name = name;
-  }
-
-  draw(color = 'black') {
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, 5, 0, Math.PI * 2);
-    ctx.fillStyle = color;
-    ctx.fill();
-    ctx.closePath();
-
-    ctx.font = "14px Arial";
-    ctx.fillStyle = color;
-    ctx.fillText(this.name, this.x - 5, this.y - 10);
-  }
-}
-
-class Edge {
-  constructor(vertex1, vertex2, name, curveFactor) {
-    this.vertex1 = vertex1;
-    this.vertex2 = vertex2;
-    this.name = name;
-    this.curveFactor = curveFactor;
-
-    const midX = (vertex1.x + vertex2.x) / 2;
-    const midY = (vertex1.y + vertex2.y) / 2;
-    const dx = vertex2.x - vertex1.x;
-    const dy = vertex2.y - vertex1.y;
-    const distance = Math.sqrt(dx * dx + dy * dy);
-
-    this.controlPoint = {
-      x: midX + curveFactor * dy / distance,
-      y: midY - curveFactor * dx / distance,
-    };
-  }
-
-  draw(color = 'black') {
-    ctx.beginPath();
-    ctx.moveTo(this.vertex1.x, this.vertex1.y);
-
-    ctx.quadraticCurveTo(this.controlPoint.x, this.controlPoint.y, this.vertex2.x, this.vertex2.y);
-    ctx.strokeStyle = color;
-    ctx.lineWidth = 2;
-    ctx.stroke();
-    ctx.closePath();
-
-    const labelPosition = this.getPointOnBezierCurve(0.5);
-    ctx.font = "14px Arial";
-    ctx.fillStyle = color;
-    ctx.fillText(this.name, labelPosition.x - 10, labelPosition.y);
-  }
-
-  /**
-   * 在贝塞尔曲线中，t 参数是一个标准化的值，范围在0到1之间。
-   * t 可以被认为是曲线上的位置，其中 t = 0 表示曲线的起点，t = 1 表示曲线的终点， t = 0.5就是弧顶。
-   */
-  getPointOnBezierCurve(t) {
-    const x = (1 - t) * (1 - t) * this.vertex1.x + 2 * (1 - t) * t * this.controlPoint.x + t * t * this.vertex2.x;
-    const y = (1 - t) * (1 - t) * this.vertex1.y + 2 * (1 - t) * t * this.controlPoint.y + t * t * this.vertex2.y;
-    return { x, y };
-  }
-
-  pointToEdgeDistance(x, y) {
-    const segments = 100;
-    let minDistance = Infinity;
-
-    for (let i = 0; i < segments; i++) {
-      const t1 = i / segments;
-      const t2 = (i + 1) / segments;
-
-      const startPoint = this.getPointOnBezierCurve(t1);
-      const endPoint = this.getPointOnBezierCurve(t2);
-
-      const dx = endPoint.x - startPoint.x;
-      const dy = endPoint.y - startPoint.y;
-      const d = Math.sqrt(dx * dx + dy * dy);
-
-      const t = ((x - startPoint.x) * dx + (y - startPoint.y) * dy) / (d * d);
-      const closestPointOnLine = t < 0 ? startPoint : t > 1 ? endPoint : {
-        x: startPoint.x + t * dx,
-        y: startPoint.y + t * dy,
-      };
-
-      const distance = Math.sqrt(Math.pow(x - closestPointOnLine.x, 2) + Math.pow(y - closestPointOnLine.y, 2));
-      minDistance = Math.min(minDistance, distance);
-    }
-
-    return minDistance;
-  }
-
-}
 
 // 具体实现游戏逻辑，例如添加顶点、边、检测点击位置等
 // ... 其他代码 ...
 
 class App {
   constructor() {
+    this.canvas = document.getElementById('drawingArea');
+    this.ctx = this.canvas.getContext('2d');
+
     this.graph = new Graph();
     this.selectedVertices = [];
 
@@ -144,11 +36,11 @@ class App {
   }
 
   init() {
-    canvas.addEventListener('click', (event) => this.handleCanvasClick(event));
+    this.canvas.addEventListener('click', (event) => this.handleCanvasClick(event));
   }
 
   handleCanvasClick(event) {
-    const rect = canvas.getBoundingClientRect();
+    const rect = this.canvas.getBoundingClientRect();
     const x = event.clientX - rect.left;
     const y = event.clientY - rect.top;
     if (this.isDrawingMode) {
@@ -205,7 +97,7 @@ class App {
       return;
     }
     const vertexName = String.fromCharCode(65 + this.vertexCounter); // 65 is ASCII code of 'A'
-    const vertex = new Vertex(x, y, vertexName);
+    const vertex = new Vertex(x, y, vertexName, this.ctx);
     this.graph.addVertex(vertex);
     vertex.draw();
   }
@@ -226,7 +118,7 @@ class App {
 
     // const curveFactor = 40 * (1 + 2 * (existingEdges.length % 2) - 1);
 
-    const edge = new Edge(vertex1, vertex2, this.edgeCounter + 1, curveFactor);
+    const edge = new Edge(vertex1, vertex2, this.edgeCounter + 1, curveFactor, this.ctx);
     this.graph.addEdge(edge);
     edge.draw();
   }
@@ -257,7 +149,7 @@ class App {
   }
 
   redraw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     this.graph.vertices.forEach(vertex => vertex.draw());
     this.graph.edges.forEach(edge => edge.draw())
   }
---
+++ graph.js
@@ -0,0 +1,120 @@
+
+class Graph {
+  constructor() {
+    this.vertices = [];
+    this.edges = [];
+  }
+
+  addVertex(vertex) {
+    this.vertices.push(vertex);
+  }
+
+  addEdge(edge) {
+    this.edges.push(edge);
+  }
+
+  // 其他方法
+}
+
+class Vertex {
+  static sortByX(...vertices) {
+    return vertices.sort((v1, v2) => v1.x - v2.x);
+  }
+
+  constructor(x, y, name, ctx) {
+    this.x = x;
+    this.y = y;
+    this.name = name;
+    this.ctx = ctx;
+  }
+
+  draw(color = 'black') {
+    this.ctx.beginPath();
+    this.ctx.arc(this.x, this.y, 5, 0, Math.PI * 2);
+    this.ctx.fillStyle = color;
+    this.ctx.fill();
+    this.ctx.closePath();
+
+    this.ctx.font = "14px Arial";
+    this.ctx.fillStyle = color;
+    this.ctx.fillText(this.name, this.x - 5, this.y - 10);
+  }
+}
+
+class Edge {
+  constructor(vertex1, vertex2, name, curveFactor, ctx) {
+    [vertex1, vertex2] = Vertex.sortByX(vertex1, vertex2)
+    this.vertex1 = vertex1;
+    this.vertex2 = vertex2;
+    this.name = name;
+    this.curveFactor = curveFactor;
+    this.ctx = ctx
+
+    const midX = (vertex1.x + vertex2.x) / 2;
+    const midY = (vertex1.y + vertex2.y) / 2;
+    const dx = vertex2.x - vertex1.x;
+    const dy = vertex2.y - vertex1.y;
+    const distance = Math.sqrt(dx * dx + dy * dy);
+
+    this.controlPoint = {
+      x: midX + curveFactor * dy / distance,
+      y: midY - curveFactor * dx / distance,
+    };
+  }
+
+  draw(color = 'black') {
+    this.ctx.beginPath();
+    this.ctx.moveTo(this.vertex1.x, this.vertex1.y);
+
+    this.ctx.quadraticCurveTo(this.controlPoint.x, this.controlPoint.y, this.vertex2.x, this.vertex2.y);
+    this.ctx.strokeStyle = color;
+    this.ctx.lineWidth = 2;
+    this.ctx.stroke();
+    this.ctx.closePath();
+
+    const labelPosition = this.getPointOnBezierCurve(0.5);
+    this.ctx.font = "14px Arial";
+    this.ctx.fillStyle = color;
+    this.ctx.fillText(this.name, labelPosition.x - 10, labelPosition.y);
+  }
+
+  /**
+   * 在贝塞尔曲线中，t 参数是一个标准化的值，范围在0到1之间。
+   * t 可以被认为是曲线上的位置，其中 t = 0 表示曲线的起点，t = 1 表示曲线的终点， t = 0.5就是弧顶。
+   */
+  getPointOnBezierCurve(t) {
+    const x = (1 - t) * (1 - t) * this.vertex1.x + 2 * (1 - t) * t * this.controlPoint.x + t * t * this.vertex2.x;
+    const y = (1 - t) * (1 - t) * this.vertex1.y + 2 * (1 - t) * t * this.controlPoint.y + t * t * this.vertex2.y;
+    return {x, y};
+  }
+
+  pointToEdgeDistance(x, y) {
+    const segments = 100;
+    let minDistance = Infinity;
+
+    for (let i = 0; i < segments; i++) {
+      const t1 = i / segments;
+      const t2 = (i + 1) / segments;
+
+      const startPoint = this.getPointOnBezierCurve(t1);
+      const endPoint = this.getPointOnBezierCurve(t2);
+
+      const dx = endPoint.x - startPoint.x;
+      const dy = endPoint.y - startPoint.y;
+      const d = Math.sqrt(dx * dx + dy * dy);
+
+      const t = ((x - startPoint.x) * dx + (y - startPoint.y) * dy) / (d * d);
+      const closestPointOnLine = t < 0 ? startPoint : t > 1 ? endPoint : {
+        x: startPoint.x + t * dx,
+        y: startPoint.y + t * dy,
+      };
+
+      const distance = Math.sqrt(Math.pow(x - closestPointOnLine.x, 2) + Math.pow(y - closestPointOnLine.y, 2));
+      minDistance = Math.min(minDistance, distance);
+    }
+
+    return minDistance;
+  }
+}
+
+export { Graph, Vertex, Edge }
